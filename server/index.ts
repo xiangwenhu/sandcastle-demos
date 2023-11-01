72a9c1695bb50b350608b4df4f7f439e9526a4b8
huxw <huxw@mail.jj.cn>
2023-11-01 16:54:30 +0800

    多个socket连接支持查看进度

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import Activity from "../../thief/src/activities/Activity";
 import { createActivity } from "../../thief/src/factory/activity";
 import getProgress from "../../thief/src/progress";
 import { EnumActivityStatus } from "../../thief/src/types/enum";
@@ -31,24 +32,35 @@ function startCaseInstance(ws: WebSocket, id: string) {
     const caseA = manager.getCase(id);
     if (!caseA) return;
 
+    let instance: Activity<any, any, any, any, any>;
     if (caseA.instances && caseA.instances.length > 0) {
-        return
-    }
-    const instance = createActivity(caseA.activityConfig);
-    caseA.instances = caseA.instances || [];
-    caseA.instances.push(instance);
+        instance = caseA.instances[0];
+        const progress = getProgress(instance);
 
-    instance.run().then(res => {
         ws.send(JSON.stringify({
-            type: "result",
-            data: res
+            type: 'progress',
+            data: {
+                id,
+                progress
+            }
         }))
-    }).finally(function () {
-        const caseA = manager.getCase(id);
-        if (caseA?.instances) {
-            caseA.instances.length = 0;
-        }
-    });
+    } else {
+        instance = createActivity(caseA.activityConfig);
+        caseA.instances = caseA.instances || [];
+        caseA.instances.push(instance);
+
+        instance.run().then(res => {
+            ws.send(JSON.stringify({
+                type: "result",
+                data: res
+            }))
+        }).finally(function () {
+            const caseA = manager.getCase(id);
+            if (caseA?.instances) {
+                caseA.instances.length = 0;
+            }
+        });
+    }
 
     instance.messenger?.on("status", function (status: EnumActivityStatus, act: any) {
         // console.log( act.type, act.name, ACTIVITY_STATUS_MAP[status])
